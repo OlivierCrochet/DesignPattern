dd50bd4b4cb448aaa24bf3c99b87768af9f9a3da
corentin <corentin.guillard@gmail.com>
2018-03-11 17:38:22 +0100

    correction du pattern decorateur finish

--- decorateur.js
+++ decorateur.js
@@ -1,14 +1,15 @@
 var Recette = {
   ListeIngredient : ["Poivre", "Carotte",]
-};
+}; //l'objet "Recette" contient toute la liste des ingrédient qu'il faut pour le plat.
 
 var DecorateurIngredient = function ajouterIngredient (Ingredient) {
     Plat.RecettePlat[Plat.RecettePlat.length] = Ingredient
-  };
+  }; //L'objet "DecorateurIngredient" comporte une fonction qui permet d'ajouter
+    //un ingrédient à la recette du plat.
 
 var Plat = {
   RecettePlat : [],
-  DecorateurIngredient : DecorateurIngredient,
+  //"PreparationPlat" récupère les ingrédients de la recette et du décorateur.
   PreparationPlat : function (Recette, IngredientSupplementaire) {
     for (var i = 0; i < Recette.ListeIngredient.length; i++) {
       Plat.RecettePlat[i] = Recette.ListeIngredient[i]
@@ -18,9 +19,9 @@ var Plat = {
     }
   };
 
-function AfficherPlat (Plat) {
+function AfficherRecettePlat (Plat) {
   for (var i = 0; i < Plat.RecettePlat.length; i++) {
     console.log(Plat.RecettePlat[i])
   }
   return "fini"
-};
+}; // la fonction "AfficherRecettePlat" permet d'afficher les ingrédients du plat.
