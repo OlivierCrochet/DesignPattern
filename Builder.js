fac2f2de4b646ce3d74d96f59dd3f4d86da1eec6
Crochet Olivier <olivier.crochet@ynov.com>
2018-03-12 17:47:54 +0100

    ajout des commentaires

--- Builder.js
+++ Builder.js
@@ -1,3 +1,4 @@
+//Vehicule est l'objet "complexe", composé de plusieurs autres objets 
 var Vehicule = {
     TypeVehicule: undefined,
     NbRoues: undefined,
@@ -6,7 +7,7 @@ var Vehicule = {
     NbPhares: undefined,
     Phares: undefined
 }
-
+//Constructeur est la fonction qui va donner à l'objet complexe ses propritétés 
 var Constructeur = function Assemblage (Plan) {
     Vehicule.TypeVehicule = Plan.TypeVehicule;
     Vehicule.NbRoues = Plan.NbRoues;
@@ -18,6 +19,7 @@ var Constructeur = function Assemblage (Plan) {
     
 }
 
+//voici les objets simple qui vont composer l'objet complexe
 var Roue = {
     type: undefined
 }
@@ -32,6 +34,7 @@ var MoteurVoiture = {
 
 var Phare = "phare"
 
+// ici, les plans font office de classe, ils donnent les instructions à la fonction pour créer l'objet complexe
 var PlanVoiture = {
     TypeVehicule: "Voiture",
     NbRoues: 4,
@@ -49,7 +52,7 @@ var PlanMoto = {
 }
 
 
-
+//fonction d'affichage, uniquement ici pour la présentation
 function AffichageVehicule (Vehicule) {
     console.log(Vehicule.TypeVehicule + ", " + Vehicule.NbRoues + " roues, Type de roues: "+ Vehicule.Roues.Type + ", " + Vehicule.NbPhares + 
     " phares, " + Vehicule.Moteur.Type )
