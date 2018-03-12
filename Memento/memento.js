6eea5131af0e4a6b7ec342985170ec0997d5ed07
Valentin Guibert <valentinguibert@hotmail.fr>
2018-03-12 18:51:04 +0100

    Program finished

--- Memento/memento.js
+++ Memento/memento.js
@@ -1,71 +1,70 @@
-var Person = function(name, street, city, state) {
-    this.name = name;
-    this.street = street;
-    this.city = city;
-    this.state = state;
-}
- 
-Person.prototype = {
- 
-    hydrate: function() {
-        var memento = JSON.stringify(this);
-        return memento;
-    },
- 
-    dehydrate: function(memento) {
-        var m = JSON.parse(memento);
-        this.name = m.name;
-        this.street = m.street;
-        this.city = m.city;
-        this.state = m.state;
+/*
+MEMENTO
+
+Le patron Memento enregistre et extériorise l’état interne d’un objet 
+pour pouvoir restaurer l’objet à un état précédant plus tard.
+
+Prenons ici l'exemple d'un éditeur de texte qui enregistre ce que vous tapez au fur et à mesure
+et qui est capable de restaurer une version précédente de votre texte si vous le souhaitez.
+
+*/
+
+// Tout d'abord nous avons notre objet memento qui va pouvoir garder l'état de l'éditeur:
+
+
+class EditorMemento {
+    constructor(content) {
+        this._content = content
+    }
+    
+    getContent() {
+        return this._content
     }
 }
- 
-var CareTaker = function() {
-    this.mementos = {};
- 
-    this.add = function(key, memento) {
-        this.mementos[key] = memento;
-    },
- 
-    this.get = function(key) {
-        return this.mementos[key];
+
+// Ensuite nous avons le Créateur de l'éditeur, qui va utiliser l'objet memento
+
+class Editor {
+    constructor(){
+        this._content = ''
+    }
+    
+    type(words) {
+        this._content = this._content + ' ' + words
+    }
+    
+    getContent() {
+        return this._content
+    }
+    
+    save() {
+        return new EditorMemento(this._content)
+    }
+    
+    restore(memento) {
+        this._content = memento.getContent()
     }
 }
- 
-// log helper
-var log = (function () {
-    var log = "";
- 
-    return {
-        add: function (msg) { log += msg + "\n"; },
-        show: function () { alert(log); log = ""; }
-    }
-})();
- 
- 
-function run() {
-    var mike = new Person("Mike Foley", "1112 Main", "Dallas", "TX");
-    var john = new Person("John Wang", "48th Street", "San Jose", "CA");
-    var caretaker = new CareTaker();
- 
-    // save state
- 
-    caretaker.add(1, mike.hydrate());
-    caretaker.add(2, john.hydrate());
- 
-    // mess up their names
- 
-    mike.name = "King Kong";
-    john.name = "Superman";
- 
-    // restore original state
- 
-    mike.dehydrate(caretaker.get(1));
-    john.dehydrate(caretaker.get(2));
- 
-    log.add(mike.name);
-    log.add(john.name);
- 
-    log.show();
-}
+
+// On pourra ensuite l'utiliser ici :
+
+const editor = new Editor()
+
+// Tapons du texte
+editor.type('Du texte.')
+editor.type('Encore du texte.')
+
+// Enregistrons l'état à restaurer : Du texte. Encore du texte.
+const saved = editor.save()
+
+// Tapon encore du texte
+editor.type('Encore et encore du texte.')
+
+
+//Sortie : Contenu avant d'avoir sauvegardé
+console.log(editor.getContent())// Du texte. Encore du texte. Encore et encore du texte.
+
+//On restaure le dernier état sauvegardé
+editor.restore(saved)
+
+console.log(editor.getContent()) // Du texte. Encore du texte.
